58b43681b943777088bb3fc2a962b3c91fa4bbe2
Yang Cheol Joo <rlaengks0001@naver.com>
2022-03-23 00:59:40 +0900

    feat: SplashActivity 생성

--- app/src/main/java/com/ycj1212/covid19vaccinationcentermap/SplashActivity.kt
+++ app/src/main/java/com/ycj1212/covid19vaccinationcentermap/SplashActivity.kt
@@ -1,11 +1,11 @@
 package com.ycj1212.covid19vaccinationcentermap
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
 }
