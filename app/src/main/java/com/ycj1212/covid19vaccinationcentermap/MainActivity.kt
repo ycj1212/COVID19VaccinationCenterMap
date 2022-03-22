b5e9e78122b169063af9c1003940748646c7b2a1
Yang Cheol Joo <rlaengks0001@naver.com>
2022-03-22 15:20:52 +0900

    init: 새 프로젝트 생성

---
+++ app/src/main/java/com/ycj1212/covid19vaccinationcentermap/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ycj1212.covid19vaccinationcentermap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
