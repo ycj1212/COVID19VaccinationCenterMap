33c673f7a790c5815cbdf6640a57e94a236015ca
Yang Cheol Joo <rlaengks0001@naver.com>
2022-03-24 00:27:13 +0900

    feat: SplashViewModel 추가

--- app/src/main/java/com/ycj1212/covid19vaccinationcentermap/SplashActivity.kt
+++ app/src/main/java/com/ycj1212/covid19vaccinationcentermap/SplashActivity.kt
@@ -1,11 +1,17 @@
 package com.ycj1212.covid19vaccinationcentermap
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.ycj1212.covid19vaccinationcentermap.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    private val splashViewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
     }
 }
---
+++ app/src/main/java/com/ycj1212/covid19vaccinationcentermap/SplashViewModel.kt
@@ -0,0 +1,9 @@
+package com.ycj1212.covid19vaccinationcentermap
+
+import androidx.lifecycle.ViewModel
+
+class SplashViewModel : ViewModel() {
+    init {
+        TODO("API 데이터 로드 후 데이터베이스에 저장")
+    }
+}
