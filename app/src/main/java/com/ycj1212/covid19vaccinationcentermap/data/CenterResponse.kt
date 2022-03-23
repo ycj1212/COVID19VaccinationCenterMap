92051a43dd207257b7a619d66b0e6a2e5b77660f
Yang Cheol Joo <rlaengks0001@naver.com>
2022-03-24 00:17:50 +0900

    feat: 코로나19 예방접종센터 DTO 추가

---
+++ app/src/main/java/com/ycj1212/covid19vaccinationcentermap/data/CenterResponse.kt
@@ -0,0 +1,49 @@
+package com.ycj1212.covid19vaccinationcentermap.data
+
+import com.google.gson.annotations.SerializedName
+
+data class CenterResponse(
+    @SerializedName("currentCount")
+    val currentCount: Int,
+    @SerializedName("data")
+    val data: List<Center>,
+    @SerializedName("matchCount")
+    val matchCount: Int,
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("perPage")
+    val perPage: Int,
+    @SerializedName("totalCount")
+    val totalCount: Int
+)
+
+data class Center(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("address")
+    val address: String,
+    @SerializedName("centerName")
+    val centerName: String,
+    @SerializedName("centerType")
+    val centerType: String,
+    @SerializedName("createdAt")
+    val createdAt: String,
+    @SerializedName("facilityName")
+    val facilityName: String,
+    @SerializedName("lat")
+    val lat: String,
+    @SerializedName("lng")
+    val lng: String,
+    @SerializedName("org")
+    val org: String,
+    @SerializedName("phoneNumber")
+    val phoneNumber: String,
+    @SerializedName("sido")
+    val sido: String,
+    @SerializedName("sigungu")
+    val sigungu: String,
+    @SerializedName("updatedAt")
+    val updatedAt: String,
+    @SerializedName("zipCode")
+    val zipCode: String
+)
