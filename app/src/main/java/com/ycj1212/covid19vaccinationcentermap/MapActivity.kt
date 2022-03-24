c5931e46df3bc12acf2210fdf30e2101464fbb32
Yang Cheol Joo <rlaengks0001@naver.com>
2022-03-24 22:16:48 +0900

    feat: 지도뷰 추가

--- app/src/main/java/com/ycj1212/covid19vaccinationcentermap/MapActivity.kt
+++ app/src/main/java/com/ycj1212/covid19vaccinationcentermap/MapActivity.kt
@@ -3,15 +3,30 @@ package com.ycj1212.covid19vaccinationcentermap
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 import com.ycj1212.covid19vaccinationcentermap.databinding.ActivityMapBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMapBinding
+    private lateinit var naverMap: NaverMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_map)
+        binding.lifecycleOwner = this
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as? MapFragment
+            ?: MapFragment.newInstance().also {
+                supportFragmentManager.beginTransaction().add(R.id.map_fragment, it).commit()
+            }
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: NaverMap) {
+        naverMap = p0
     }
 }
