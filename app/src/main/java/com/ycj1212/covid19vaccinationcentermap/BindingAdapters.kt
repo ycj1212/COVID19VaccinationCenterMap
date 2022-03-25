406542a020006b22106c2d2949cd625927703718
Yang Cheol Joo <rlaengks0001@naver.com>
2022-03-25 21:09:36 +0900

    feat: Databinding 적용

--- app/src/main/java/com/ycj1212/covid19vaccinationcentermap/BindingAdapters.kt
+++ app/src/main/java/com/ycj1212/covid19vaccinationcentermap/BindingAdapters.kt
@@ -1,9 +1,19 @@
 package com.ycj1212.covid19vaccinationcentermap
 
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ProgressBar
+import androidx.core.view.updateLayoutParams
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("app:progressTo")
 fun progressTo(progressBar: ProgressBar, progress: Int) {
     progressBar.progress = progress
+}
+
+@BindingAdapter("layoutMarginBottom")
+fun setLayoutMarginBottom(view: View, dimen: Float) {
+    view.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+        bottomMargin = dimen.toInt()
+    }
 }
